32483b88b5c4aac14d8b06e0e6f2ac79dcdf6a50
isamartinezmoure <isamartinezmoure@gmail.com>
2018-07-10 15:49:29 +0000

    Done.

---
+++ fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz(number)
+  
+  if (number %3 == 0)&& (nukmber %5== 0)
+    puts "fizzbuzz"
+    
+  elsif (number %3 == 0)
+      puts "fizz"
+      
+  elsif (number %5 == 0)
+      puts "buzz"
+  end 
+  
+end 
+      
+      
+      
